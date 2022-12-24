130b28d13dd1faadc0c1710f78149c5ae85d43cb
Theflash444123 <mohit444123@gmail.com>
2022-12-24 19:00:08 +0000

    Creating dataframe dictionary of raw data
    all the files with ".csv" extension inside the data folder are parsed to dataframe and stored in a dictionary.

--- CW_Preprocessing.py
+++ CW_Preprocessing.py
@@ -1,7 +1,22 @@
 import sqlite3
 import pandas as pd
+import os
 
-module1_df = pd.read_csv("cop504cwdata/22COA111ModuleRegister.csv")
-module2_df = pd.read_csv("cop504cwdata/22COA122ModuleRegister.csv")
+# keeping all the raw dataframes in a dictionary
+count = 0
+module_register_dict = {}
+for root, dirs, files in os.walk(os.path.abspath("cop504cwdata")):
+    for file in files:
+        if file.endswith(".csv"):
+            file_path = os.path.join(root, file)
+            module_register_dict[count] = {
+                "name": file,
+                "df": pd.read_csv(file_path)
+            }
+            count += 1
+
+
+
+
 
 
